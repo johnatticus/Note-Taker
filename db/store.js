a4025f89f9ad1c08f6856d888e7a3aca908bfb69
John Hysong <johnahysong@gmail.com>
2022-04-27 20:59:15 -0400

    added store.js file

---
+++ db/store.js
@@ -0,0 +1,26 @@
+// require the util and fs needed
+const util = require('util');
+const fs = require('fs');
+
+// require the uuid/v1 package in your package.json
+const uuidv1 = require('uuid/v1');
+
+// write to file
+
+// read to file
+
+class Store {
+    read() {
+        
+    }
+
+    write() {
+
+    }
+
+    // get notes
+    // add notes
+    // delete notes which isn't required
+}
+
+module.exports = new Store();
---
+++ routes/apiRoutes.js
@@ -0,0 +1,7 @@
+// require router and db items needed
+
+
+// set up get/post/delete methods as responses to the database
+
+// export to the router
+module.exports = router;
